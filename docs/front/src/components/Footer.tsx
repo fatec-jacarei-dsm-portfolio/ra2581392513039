d65832aaa37ada8fce91b36035bbf418257c770f
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-09-09 19:18:12 -0300

    deploy

--- docs/front/src/components/Footer.tsx
+++ docs/front/src/components/Footer.tsx
@@ -5,13 +5,13 @@ export default function Footer() {
     return (
         <div className="footer">
             <div className="footer-icons">
-                <a href="">
+                <a href="https://www.instagram.com/thech0sen0n3_">
                     <img src="../public/icons8-instagram-logo.svg" alt="Instagram" />
                 </a>
-                <a href="">
+                <a href="https://github.com/MarcioBuenoo">
                     <img src="../public/icons8-github.svg" alt="GitHub" />
                 </a>
-                <a href="">
+                <a href="https://www.linkedin.com/in/marcio-roberto-bueno-junior-02a2a4248">
                     <img src="../public/icons8-linkedin.svg" alt="LinkedIn" />
                 </a>
             </div>
