b976d64b0033a27781b039965d802600bd241fee
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-09-04 19:43:45 -0300

    feat: working on header component

--- docs/src/components/Header.tsx
+++ docs/src/components/Header.tsx
@@ -1,10 +1,10 @@
 function Header() {
     return (
         <div className="bg-zinc-500/5 flex justify-center gap-20 p-8">
-            <a href="">Sobre mim</a>
-            <a href="">Tecnologias</a>
-            <a href="">Projetos</a>
-            <a href="">Contato</a>
+            <a href="" className="font-mono font-bold text-red-700">Sobre mim</a>
+            <a href="" className="font-mono font-bold text-red-700">Tecnologias</a>
+            <a href="" className="font-mono font-bold text-red-700">Projetos</a>
+            <a href="" className="font-mono font-bold text-red-700">Contato</a>
         </div>
     )
 }
