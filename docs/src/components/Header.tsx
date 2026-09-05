f89b326cfab083f1689be91e49725ee579bd10a3
MarcinBuenoo <marcinbueno01@hotmail.com>
2026-09-05 17:31:27 -0300

    feat: working on Header content

--- docs/src/components/Header.tsx
+++ docs/src/components/Header.tsx
@@ -1,10 +1,10 @@
 function Header() {
     return (
-        <div className="bg-zinc-500/5 flex justify-center gap-20 p-8">
-            <a href="" className="font-mono font-bold text-red-700">Sobre mim</a>
-            <a href="" className="font-mono font-bold text-red-700">Tecnologias</a>
-            <a href="" className="font-mono font-bold text-red-700">Projetos</a>
-            <a href="" className="font-mono font-bold text-red-700">Contato</a>
+        <div className="bg-zinc-500/5 flex justify-center gap-20 p-8 shadow-xl">
+            <a href="" className="text-lg font-mono font-bold text-red-700 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">Sobre mim</a>
+            <a href="" className="text-lg font-mono font-bold text-red-700 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">Tecnologias</a>
+            <a href="" className="text-lg font-mono font-bold text-red-700 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">Projetos</a>
+            <a href="" className="text-lg font-mono font-bold text-red-700 transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">Contato</a>
         </div>
     )
 }
