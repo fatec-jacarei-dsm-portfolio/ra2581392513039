be6263f3587cd037254eb08c49812e74ff248fd7
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-09-14 20:04:17 -0300

    feat: adding stacks icons to assets

--- docs/src/components/SobreMim.tsx
+++ docs/src/components/SobreMim.tsx
@@ -7,7 +7,7 @@ function SobreMim() {
                 Olá, me chamo Márcio e sou Desenvolvedor de Software. Sou fã de computadores e e jogos desde pequeno, <br /> oque me fez ter interesse em programção
                 Hoje sou graduando no curso de Desenvolvimento de Software Multiplataforma <br />pela FATEC Jacareí. Sou proficiente em front-end, back-end e na área de dados.
             </p>
-            <img className="float-right rounded-md h-70 w-70 shadow-xl" src={profilePhoto} alt="" />
+            <img className="float-right rounded-md h-70 w-70 shadow-xl transition delay-100 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100" src={profilePhoto} alt="" />
         </div>
     )
 }
--- docs/src/components/Tecnologias.tsx
+++ docs/src/components/Tecnologias.tsx
@@ -9,27 +9,35 @@ function Tecnologias() {
             <div className="flex grid grid-cols-4 gap-4">
                 <a href="">
                     <img src="" alt="" />
+                    Typescript
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    React.js
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    Tailwind.css
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    Docker
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    Flutter
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    PostgreSQL
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    MongoDB
                 </a>
                 <a href="">
                     <img src="" alt="" />
+                    Godot
                 </a>
             </div>
         </div>
