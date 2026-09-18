2d736d424554cee845b17e7b3a56beef2d87d860
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-09-17 22:04:30 -0300

    feat: adding projects section

--- docs/src/components/Projetos.tsx
+++ docs/src/components/Projetos.tsx
@@ -24,11 +24,12 @@ const projetos: Projeto[] = [
 
 function Projetos() {
     return (
-        <div>
+        <div className="flex items-center justify-center flex-col gap-10">
             <h1>
                 Projetos
             </h1>
-            <div id="projetos-list">
+            <div id="projetos-list" 
+            className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols4 gap-6 w-full max-w-6xl mx-auto px-4" >
                 {projetos.map((projeto) => (
                 <ProjetoCard
                     key={projeto.id}
