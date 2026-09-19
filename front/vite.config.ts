39c923f5ea65f37f5571ed1e964a04a0728271c1
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-09-18 21:16:47 -0300

    feat: adding contact section

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ra2581392513039/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
